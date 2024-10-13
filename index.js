709c0711898f9ce7de865dfd7f5b279fc4377a3a
Anson <ansonox@gmail.com>
2024-10-13 01:38:01 +0100

    fix port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/log-ip", (req, res) => {
   res.send(`[${counter}] Your IP address is logged: ${requesterIp}`);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
